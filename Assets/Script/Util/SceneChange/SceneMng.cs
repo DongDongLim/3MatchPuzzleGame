ee64a640329cf32f51723d9e9f9e7dd68100a030
Donghyen-Lim <99638155+DongDongLim@users.noreply.github.com>
2022-09-15 16:43:26 +0900

    feat : Add SceneMng
    씬 이동 및 이동간 이벤트를 담당하는 씬 매니저 생성

---
+++ Assets/Script/Util/SceneChange/SceneMng.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneMng : Singleton<SceneMng>
+{
+    IEnumerator iter;
+
+    // 씬 Enter 이벤트
+    public UnityAction<string> SceneEnter;
+
+    // 씬 Exit 이벤트
+    public UnityAction<string> SceneExit;
+
+    // 씬 Move 이벤트
+    public UnityAction<string> SceneMove;
+
+    // 현재 씬
+    public Scene curScene;
+
+    // 로딩 캔버스
+    [SerializeField]
+    GameObject loadingCanvas;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        // 현재 씬 받아오기
+        curScene = SceneManager.GetActiveScene();
+    }
+
+    public void SceneChange(string sceneName)
+    {
+        iter = LoadYourAsyncScene(sceneName);
+        StartCoroutine(iter);
+    }
+
+    public void SceneChange(int sceneIndex)
+    {
+        SceneChange(SceneManager.GetSceneByBuildIndex(sceneIndex).name);
+    }
+
+    IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        SceneExit?.Invoke(curScene.name);
+        while (!asyncLoad.isDone)
+        {
+            SceneMove?.Invoke(sceneName);
+            yield return null;
+        }
+        curScene = SceneManager.GetActiveScene();
+        SceneEnter?.Invoke(sceneName);
+    }
+}
