423d1002a1bcf1827501d5dc12e1b75a9c51dd50
Donghyen-Lim <99638155+DongDongLim@users.noreply.github.com>
2022-09-23 19:12:10 +0900

    feat : Add NetworkPlayer
    네트워크 플레이서 생성

--- Assets/Script/Photon/BasicSpawner.cs
+++ Assets/Script/Photon/BasicSpawner.cs
@@ -12,6 +12,8 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
     [SerializeField]
     private NetworkRunner _runner;
 
+    public NetworkPlayer m_playerPrefab;
+
     private void OnGUI()
     {
         if (_runner == null)
@@ -43,83 +45,43 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
     }
 
 
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-        
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner)
-    {
-        
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-        
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-        
-    }
-
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        
+        if(runner.IsServer)
+        {
+            Debug.Log("플레이어 조인");
+            runner.Spawn(m_playerPrefab, Vector2.zero, Quaternion.identity, player);
+        }
     }
 
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
-        
-    }
 
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
-    {
-        
-    }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
 
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-        
-    }
+    public void OnConnectedToServer(NetworkRunner runner) { }
 
-    public void OnSceneLoadStart(NetworkRunner runner)
-    {
-        
-    }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
 
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-        
-    }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
 
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-        
-    }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
 
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-        
-    }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
 }
